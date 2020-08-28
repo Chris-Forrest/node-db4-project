dea39f97d1f664ba70171f6f23c7cbe41caa6ff1
Chris Forrest <redrider_cr@yahoo.com>
2020-08-28 15:32:59 -0400

    3 stretch endpoints done added logger function and error handling middleware

--- model-router/recipes-router.js
+++ model-router/recipes-router.js
@@ -4,30 +4,33 @@ const Recipes = require("./recipes-model");
 
 const router = express.Router();
 
-router.get("/", async (req,res) => {
+router.get("/", async (req,res,next) => {
     try{
         const recipes = await Recipes.getRecipes()
         res.status(200).json(recipes)
     }catch(err){
-        res.status(500).json({ message: "Could not get recipes."})
+        //res.status(500).json({ message: "Could not get recipes."})
+        next(err)
     }
 });
 
-router.get("/:id/shopping-list", async (req, res) => {
+router.get("/:id/shopping-list", async (req, res,next) => {
     try{
     const list = await Recipes.getShoppingList(req.params.id)
     res.status(200).json(list)
     }catch(err){
-        res.status(500).json({ message:"Could not get shopping list"})
+       // res.status(500).json({ message:"Could not get shopping list"})
+       next(err)
     }
 });
 
-router.get("/:id/instructions", async (req,res) => {
+router.get("/:id/instructions", async (req,res,next) => {
     try{
         const instructions = await Recipes.getInstructions(req.params.id)
         res.status(200).json(instructions)
     }catch(err){
-        res.status(500).json({ message: "Could not get instructions"})
+       // res.status(500).json({ message: "Could not get instructions"})
+       next(err)
     }
 });
 
