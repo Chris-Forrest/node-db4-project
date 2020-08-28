a2e5b53773385f7f73b01ff1078351bc1534ac75
Chris Forrest <redrider_cr@yahoo.com>
2020-08-28 15:16:49 -0400

    mvp met get shoppingList and instructions working

--- model-router/recipes-router.js
+++ model-router/recipes-router.js
@@ -13,5 +13,23 @@ router.get("/", async (req,res) => {
     }
 });
 
+router.get("/:id/shopping-list", async (req, res) => {
+    try{
+    const list = await Recipes.getShoppingList(req.params.id)
+    res.status(200).json(list)
+    }catch(err){
+        res.status(500).json({ message:"Could not get shopping list"})
+    }
+});
+
+router.get("/:id/instructions", async (req,res) => {
+    try{
+        const instructions = await Recipes.getInstructions(req.params.id)
+        res.status(200).json(instructions)
+    }catch(err){
+        res.status(500).json({ message: "Could not get instructions"})
+    }
+});
+
 
 module.exports = router;
