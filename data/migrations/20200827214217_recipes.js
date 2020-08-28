58d8d4a54dc4d3c9da71b4040a1849033a6abe89
Chris Forrest <redrider_cr@yahoo.com>
2020-08-28 08:01:22 -0400

    made ingredients and instructions tables

--- data/migrations/20200827214217_recipes.js
+++ data/migrations/20200827214217_recipes.js
@@ -5,9 +5,30 @@ exports.up = function(knex) {
       tbl.increments()
       tbl.text("recipe_name", 255).notNullable()
   })
+
+  .createTable("ingredients", (tbl) => {
+      tbl.increments()
+      tbl.text("ingredient_name", 255).notNullable()
+  })
+
+  .createTable("instructions", (tbl) => {
+      tbl.increments()
+      tbl.string("description").notNullable()
+      tbl.integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE")
+  })
+
+  
 };
 
 exports.down = function(knex) {
   return knex.schema
+  .dropTableIfExists("instructions")
+  .dropTableIfExists("ingredients")
   .dropTableIfExists("recipes")
 };
