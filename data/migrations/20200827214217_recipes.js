d641e4d69873df7f78a7edf7c1e443b55674bd76
Chris Forrest <redrider_cr@yahoo.com>
2020-08-28 08:14:47 -0400

    made recipes_ingredients table

--- data/migrations/20200827214217_recipes.js
+++ data/migrations/20200827214217_recipes.js
@@ -13,6 +13,9 @@ exports.up = function(knex) {
 
   .createTable("instructions", (tbl) => {
       tbl.increments()
+      tbl.integer("step_number")
+        .unsigned()
+        .notNullable()
       tbl.string("description").notNullable()
       tbl.integer("recipe_id")
         .unsigned()
@@ -23,11 +26,34 @@ exports.up = function(knex) {
         .onDelete("CASCADE")
   })
 
-  
+  .createTable("recipes_ingredients", (tbl) => {
+      tbl.float("quantity")
+        .unsigned()
+        .notNullable()
+    tbl.integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("recipes")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE")
+    tbl.integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("ingredients")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE")
+
+        tbl.primary(["recipe_id", "ingredient_id"])
+  })
+
+
 };
 
 exports.down = function(knex) {
   return knex.schema
+  .dropTableIfExists("recipes_ingredients")
   .dropTableIfExists("instructions")
   .dropTableIfExists("ingredients")
   .dropTableIfExists("recipes")
